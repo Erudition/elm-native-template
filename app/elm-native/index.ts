b32ce888f949b0da1aa05e04e3c757af7cfca325
Erudition <cnnr_dhrty@live.com>
2019-09-22 01:49:22 -0400

    try to restore navigation

--- app/elm-native/index.ts
+++ app/elm-native/index.ts
@@ -10,22 +10,23 @@ declare global {
     }
 }
 
-export function elmNative(elmApp: typeof ElmComponent, data: any): Promise<ElmComponent> {
+export function elmNative(startPage: typeof ElmComponent, data: any): Promise<ElmComponent> {
     initializeDom();
     console.warn("HEY I'M IN elmNative and doc is " + window.document);
     //setup a frame so we always have somewhere to hang our css
     let rootFrame = createElement('frame') as FrameElement;
     rootFrame.setAttribute("id", "app-root-frame");
 
-    // let pageInstance = navigate({
-    //     page: startPage,
-    //     props: data || {},
-    //     frame: rootFrame
-    // })
-    elmApp = require("../compiled-elm.js").Elm.Main;
-    var pageInstance = elmApp.init({
+
+    var runningElmApp = require("../compiled-elm.js").Elm.Main.init({
       node: rootFrame
     });
+    let pageInstance = navigate({
+        page: rootFrame, //was startPage
+        props: data || {},
+        frame: rootFrame
+    })
+
 
     return new Promise((resolve, reject) => {
         //wait for launch
